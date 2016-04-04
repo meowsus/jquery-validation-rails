78a80200ec1d1bdc4f4e8f9fff27473fa8a74a7d
Ryan Durel <rdurel423@gmail.com>
2016-04-04 15:28:12 -0500

    Add v1.15.0 Localizations

--- app/assets/javascripts/jquery.validate.localization/messages_zh_TW.js
+++ app/assets/javascripts/jquery.validate.localization/messages_zh_TW.js
@@ -1,17 +1,9 @@
-(function( factory ) {
-	if ( typeof define === "function" && define.amd ) {
-		define( ["jquery", "../jquery.validate"], factory );
-	} else {
-		factory( jQuery );
-	}
-}(function( $ ) {
-
 /*
  * Translated default messages for the jQuery validation plugin.
  * Locale: ZH (Chinese; 中文 (Zhōngwén), 汉语, 漢語)
  * Region: TW (Taiwan)
  */
-$.extend($.validator.messages, {
+$.extend( $.validator.messages, {
 	required: "必須填寫",
 	remote: "請修正此欄位",
 	email: "請輸入有效的電子郵件",
@@ -23,12 +15,10 @@ $.extend($.validator.messages, {
 	creditcard: "請輸入有效的信用卡號碼",
 	equalTo: "請重複輸入一次",
 	extension: "請輸入有效的後綴",
-	maxlength: $.validator.format("最多 {0} 個字"),
-	minlength: $.validator.format("最少 {0} 個字"),
-	rangelength: $.validator.format("請輸入長度為 {0} 至 {1} 之間的字串"),
-	range: $.validator.format("請輸入 {0} 至 {1} 之間的數值"),
-	max: $.validator.format("請輸入不大於 {0} 的數值"),
-	min: $.validator.format("請輸入不小於 {0} 的數值")
-});
-
-}));
+	maxlength: $.validator.format( "最多 {0} 個字" ),
+	minlength: $.validator.format( "最少 {0} 個字" ),
+	rangelength: $.validator.format( "請輸入長度為 {0} 至 {1} 之間的字串" ),
+	range: $.validator.format( "請輸入 {0} 至 {1} 之間的數值" ),
+	max: $.validator.format( "請輸入不大於 {0} 的數值" ),
+	min: $.validator.format( "請輸入不小於 {0} 的數值" )
+} );
